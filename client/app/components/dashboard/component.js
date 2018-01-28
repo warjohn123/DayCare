c852b6b3980094905372b464ec1932e658a10189
warjohn123 <warrencaruana1@gmail.com>
2018-01-28 14:42:27 +0800

    Add children files

---
+++ client/app/components/dashboard/component.js
@@ -0,0 +1,11 @@
+import template from './template.html';
+import controller from './controller';
+import './styles.scss';
+
+let dashboardComponent = {
+  bindings: {},
+  template,
+  controller
+};
+
+export default dashboardComponent;
